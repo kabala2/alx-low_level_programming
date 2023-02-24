5706f48a989ece9d60a9eb6b449f29fd41d0e895
Abayomi Dauda <aabdulafeezdauda.com>
2023-02-23 18:19:35 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - function that prints a sqare
+ *@size: size of both widith and length
+ * Return: square made of '#'
+ */
+void print_square(int size)
+{
+int co, ro;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (co = 1; co <= size; co++)
+{
+_putchar('#');
+for (co = 2; co <= size; ro++)
+{
+_putchar('#');
+
+}
+_putchar('\n');
+}
+}
+}
