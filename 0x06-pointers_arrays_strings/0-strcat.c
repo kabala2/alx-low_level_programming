852561e0e3762e7dc58f214072e25b3b54973547
Abayomi Dauda <aabdulafeezdauda.com>
2023-03-02 04:54:13 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two stings
+ * @dest: input value
+ * @src: input value
+ * Return: voild
+ */
+char *_strcat(char *dest, char *src)
+{
+int i;
+int j;
+i = 0;
+while (dest[i] != '\0')
+{
+i++
+}
+j = 0;
+while (scr[j] != '\0')
+{
+dest[i] = scr[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
